2b4d20dc091994df14a4f5153dc7bb4c5d4ed3a2
Carlos Moreno <omorenodovale@gmail.com>
2022-03-05 17:03:58 -0300

    feat: Resolução dos exercícios 1 e 2 da lista 1 do curso Python para Zumbis.

---
+++ pythonparazumbis/exercicio01.py
@@ -0,0 +1,7 @@
+print("Informe dois números inteiros.")
+num_1 = int(input("Informe o primeiro número: "))
+num_2 = int(input("Informe o segundo número: "))
+
+resultado = num_1 + num_2
+
+print(f"A soma dos números informados é {resultado}")
---
+++ pythonparazumbis/exercicio02.py
@@ -0,0 +1,2 @@
+metros = float(input("Metros: "))
+print(f"{metros:.2f} metros equivale a {metros * 1000:.2f} milímetros")
