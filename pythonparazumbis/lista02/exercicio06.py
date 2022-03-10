5ea524193fbe2afa76af1aa9fb6c03c731fe1bf6
Carlos Moreno <omorenodovale@gmail.com>
2022-03-10 09:17:49 -0300

    feat: Resolução do exercício 7 da lista 2 do curso Python para Zumbis.

--- pythonparazumbis/lista02/exercicio06.py
+++ pythonparazumbis/lista02/exercicio06.py
@@ -8,7 +8,7 @@ sind = bruto * 0.05
 liquido = bruto - (ir + inss + sind)
 
 print(f"+Salário Bruto: \t R$ {bruto:.2f}")
-print(f"-IR: \t\t\t\t R$ {ir:.2f}")
-print(f"-INSS: \t\t\t\t R$ {inss:.2f}")
-print(f"-Sindicato: \t\t R$ {sind:.2f}")
-print(f"Salário Líquido: \t R$ {liquido:.2f}")
+print(f"-IR (11%): \t\t\t R$ {ir:.2f}")
+print(f"-INSS (8%): \t\t R$ {inss:.2f}")
+print(f"-Sindicato (5%): \t R$ {sind:.2f}")
+print(f"=Salário Líquido: \t R$ {liquido:.2f}")
